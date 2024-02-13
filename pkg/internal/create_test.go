d480108f1abc350cf5ea7b98ce8397af41675b1c
Aidan Delaney <adelaney21@bloomberg.net>
2024-02-13 09:43:18 +0000

    Switch to require for tests (#20)
    Fail fast if a test failure occurs

--- pkg/internal/create_test.go
+++ pkg/internal/create_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 
 	"github.com/sclevine/spec"
-	h "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/buildpacks-community/scafall/pkg/internal"
 )
@@ -22,9 +22,9 @@ func testCreate(t *testing.T, when spec.G, it spec.S) {
 			inputDir, _ = os.MkdirTemp("", "test")
 			targetDir, _ = os.MkdirTemp("", "test")
 			file, err := os.Create(filepath.Join(inputDir, "test.md"))
-			h.Nil(t, err)
+			require.Nil(t, err)
 			_, err = file.WriteString("{{.Test}}")
-			h.Nil(t, err)
+			require.Nil(t, err)
 		})
 
 		it.After(func() {
@@ -34,26 +34,26 @@ func testCreate(t *testing.T, when spec.G, it spec.S) {
 
 		it("creates valid output", func() {
 			err := internal.Create(inputDir, map[string]string{"Test": "quack"}, targetDir)
-			h.Nil(t, err)
+			require.Nil(t, err)
 
 			buf, err := os.ReadFile(filepath.Join(targetDir, "test.md"))
-			h.Nil(t, err)
-			h.Equal(t, string(buf), "quack")
+			require.Nil(t, err)
+			require.Equal(t, string(buf), "quack")
 		})
 
 		when("a prompt.toml file is present", func() {
 			it.Before(func() {
 				_, err := os.Create(filepath.Join(inputDir, "prompts.toml"))
-				h.Nil(t, err)
+				require.Nil(t, err)
 			})
 
 			it("reads prompt.toml and creates valid output", func() {
 				err := internal.Create(inputDir, map[string]string{"Test": "quack"}, targetDir)
-				h.Nil(t, err)
+				require.Nil(t, err)
 
 				buf, err := os.ReadFile(filepath.Join(targetDir, "test.md"))
-				h.Nil(t, err)
-				h.Equal(t, string(buf), "quack")
+				require.Nil(t, err)
+				require.Equal(t, string(buf), "quack")
 			})
 		})
 	})
